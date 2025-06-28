dd5107a500e99459ca1e628e820ebe53607298db
Eng99Arjun <dhananjay.99jb@gmail.com>
2025-06-28 23:00:31 +0530

    feat: implement reusable Input component and update form structure

--- react-form/src/App.jsx
+++ react-form/src/App.jsx
@@ -1,34 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const Input = (props) => {
+  return <div className='m-2'>
+    <div>
+      <label htmlFor={props.fieldname}>{`${props.fieldname}`}</label>
+    </div>
+    <div>
+      <input type={props.type} placeholder={`Enter ${props.fieldname}`} className='outline-1 outline-gray-400 w-xl rounded-xl p-1' />
+    </div>
+    
+  </div>
+}
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='m-auto mt-10 shadow-2xl h-1/2 w-fit p-5 rounded-3xl '>
+      <h1 className='text-2xl font-bold text-green-500 text-center'>
+        Form in React
+      </h1>
+      <Input type='text' fieldname= 'First Name ' />
+      <Input type='text' fieldname= 'Last Name' />
+      <Input type='email' fieldname= 'Email' />
+      <Input type='number' fieldname= 'Contact' />
+      <div className='m-2'>
+        <div>
+          <label htmlFor="Gender"> Gender</label>
+        </div>
+        <div>
+          <span className='m-3'>
+            <input type="radio" name="gender" id="male" /> Male      
+          </span>
+          <span className='m-3'>
+            <input type="radio" name="gender" id="female" /> Female     
+          </span>
+          <span>
+            <input type="radio" name="gender" id="other" /> Other     
+          </span>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      {/* Your Best Subject */}
+      <div className='m-2'>
+        <div>
+          <label htmlFor="bestSubject"> Your Best Subject </label>
+        </div>
+        <div>
+          <span className='m-3'>
+            <input type="checkbox" name="subject" id="english" /> English      
+          </span>
+          <span className='m-3'>
+            <input type="checkbox" name="subject" id="maths" /> Maths     
+          </span>
+          <span>
+            <input type="checkbox" name="subject" id="physics" /> Physics 
+          </span>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      {/* Resume */}
+
+      <div className='m-2'>
+        <div>Upload Resume</div>
+        <input type="file" name="resume" id="resume" className='outline w-xl p-1 rounded-xl '/>
+      </div>
+
+    </div>
   )
 }
 
