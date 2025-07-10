5843e327284424a3a8a8dfe7a3a0b8954fa94c4a
Eng99Arjun <dhananjay.99jb@gmail.com>
2025-07-10 16:10:39 +0530

    feat: implement todo list functionality with input handling and state management

--- ToDo/src/App.jsx
+++ ToDo/src/App.jsx
@@ -1,34 +1,40 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [description, setDescription] = useState("");
+  const [input, setInput] = useState('');
+  const [todos, setTodos] = useState([]);
+
+
+  const addTodo =() => {
+    if(!input.trim()) return;
+    const newTodo = {id: Date.now(), text: input.trim(), description: description.trim(), completed: false};
+    setTodos([newTodo, ...todos]);
+    setInput('');
+    setDescription('');
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <input type="text"
+        placeholder='Enter a Task'
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        />
+      <input type="text"
+        placeholder='Enter description'
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+        />
+        <button onClick={addTodo}>Add Todo</button>
+
+        <div>
+          {todos.map((todo) => (
+            <h3>{todo.text}</h3>
+          ))}
+        </div>
+    </div>
   )
 }
 
