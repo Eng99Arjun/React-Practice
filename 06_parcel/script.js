68d8475312130a5003e25687bcecbcd45673420c
Eng99Arjun <dhananjay.99jb@gmail.com>
2025-08-03 22:45:09 +0530

    create

---
+++ 06_parcel/script.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+console.log("Hello World");
+console.log(React);
+
+const h1 = <h1>Hello World</h1>
+root.render('h1');
